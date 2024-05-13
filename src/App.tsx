f9457c9d6e21320453beccc82fd377ab03af8104
João Leandro <joao.oliveira1270@etec.sp.gov.br>
2024-05-13 16:29:05 -0300

    feat: button scroll to top, social buttons

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,18 @@ import { Loader2Icon } from "lucide-react";
 import { PokeCard } from "./components/PokeCard/PokeCard";
 import { usePokeList } from "./hooks/usePokeList/usePokeList";
 import { Container } from "./styles/styles";
+import pikachu from '/images/pikachu.svg'
+import { Linkedin, Github } from 'lucide-react'
 
 export function App() {
   const { PokeList, isLoading, handleOffsetValue } = usePokeList()
+
+  const handleScrolltoTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    })
+  }
 
   return (
     <>
@@ -19,9 +28,23 @@ export function App() {
           <Loader2Icon size={200} color="#FFFFFF" />
         </div>
       }
-      {isLoading ? '' : <button onClick={handleOffsetValue}>
-        Carregar mais pokemons
-      </button>}
+      {isLoading
+        ? ''
+        : <div className="loadingButton">
+          <div className="social">
+            <a href="https://www.linkedin.com/in/jvictorlndr/" target="_blank">
+              <Linkedin size={35} strokeWidth={1.3} />
+            </a>
+            <a href="https://github.com/Humildeficador" target="_blank">
+              <Github size={35} strokeWidth={1.3} />
+            </a>
+          </div>
+          <button onClick={handleOffsetValue}>
+            Carregar mais pokemons
+          </button>
+          <img src={pikachu} alt="Pikachu" onClick={handleScrolltoTop} />
+        </div>
+      }
     </>
   )
 }
